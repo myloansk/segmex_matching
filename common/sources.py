9da82c607b103e7a1801e87ec5ad06abbfa4bc83
myloansk <cosmyl77@gmail.com>
2022-11-23 17:42:48 +0200

    Adding PoiData responsible for reading and preparing data coming from external vendors

--- common/sources.py
+++ common/sources.py
@@ -17,3 +17,18 @@ class Source(ABC):
     @abstractmethod
     def prepareData(self)->DataFrame:pass
 
+class PoiData(Source):
+
+    _sourceConfig:_externalSrcConfig
+
+    def readData(self)->DataFrame:
+        df_from_each_file = (pd.read_csv(f, low_memory=False) for f in [self._externalSrcConfig.poi_filepath[self._cc]])
+        poiListingPd   = pd.concat(df_from_each_file, ignore_index=True)
+
+        # Convert to spark dataframe
+        return spark.createDataFrame(poiListingPd)
+
+    def prepareData(self) -> DataFrame:
+        return super().prepareData()
+
+

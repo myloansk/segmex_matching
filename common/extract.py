783332a0494eff3a5850686bb36961ae1ae3a3ea
myloansk <cosmyl77@gmail.com>
2023-01-02 15:52:58 +0200

    Refactoring code

--- common/extract.py
+++ common/extract.py
@@ -6,7 +6,9 @@ import pandas as pd
 from builtins import sum as pysum
 from pyspark.sql import DataFrame
 from pyspark.sql.window import Window
-from typing import Dict, List, NamedTuple,Optional
+from typing import ( Dict, List,
+                    NamedTuple, Optional
+)
 
 class InternalSrcConfig(NamedTuple):
   selected_column_list:List[str]
@@ -50,89 +52,111 @@ class ExternalSrcConfig(NamedTuple):
   
 country = 'at'
 class ExtractData():
-    def __init__(self, internalSrcConfig:InternalSrcConfig=None,externalSrcConfig:ExternalSrcConfig = None, cc:str = country )->None:
-        self._internalSrcConfig = internalSrcConfig
-        self._externalSrcConfig = externalSrcConfig
+    def __init__(self, srcConfig:NamedTuple = None, cc:str = country )->None:
+        #self._internalSrcConfig = internalSrcConfig
+        #self._externalSrcConfig = externalSrcConfig
+        self._srcConfig = srcConfig
         self._cc = cc
       
 
-        self._internalDf:DataFrame = None
-        self._externalDf:DataFrame = None
-        self._auxiliaryDf:DataFrame = None
+        #self._internalDf:DataFrame = None
+        #self._externalDf:DataFrame = None
+        #self._auxiliaryDf:DataFrame = None
       
-    def get_auxiliary_data(self)->DataFrame:
-      return self._auxiliaryDf
+    #def get_auxiliary_data(self)->DataFrame:
+    #  return self._auxiliaryDf
 
-    def set_auxiliary_data(self, df:DataFrame)->None:
-      self._auxiliaryDf = df
+    #def set_auxiliary_data(self, df:DataFrame)->None:
+    #  self._auxiliaryDf = df
 
-    def get_internal_source_config(self)->InternalSrcConfig:
-      return self._internalSrcConfig
+    def get_source_config(self)->NamedTuple:
+      return self._srcConfig
+
+    def set_source_config(self, srcConfig:NamedTuple)->None:
+      self._srcConfig = srcConfig
     
-    def set_internal_source_config(self, internalSrcConfig:InternalSrcConfig)->None:
-      self._internalSrcConfig = internalSrcConfig
+    def extract_cchbc_data(self)->DataFrame:pass
+
+    def extract_poi_data(self)->DataFrame:
+      poiData = (PoiData()
+                  .setConfig(self._srcConfig))
       
-    def get_external_source_config(self)->ExternalSrcConfig:
-      return  self._externalSrcConfig
+      (poiData.readData()
+            .prepareAuxiliaryData()
+            .prepareData())
+
+      return poiData._sparkDf
     
-    def set_external_source_config(self)->None:
-      self._externalSrcConfig = externalSrcConfig
+    def extract_cchbc_data(self)->DataFrame:
+      cchbcData = (CCHBCData.setConfig(self._srcConfig))
 
-    def get_internal_data(self)->DataFrame:
-          # Load customer MD
-          customerDf = spark.read.option("header", "true").option("sep", "|").csv(self._internalSrcConfig.filepaths['customer'].format( CAPS_CC = self._cc.upper()))
-          customerDf = customerDf.fillna("NAN", subset=['_BIC_CSUP_CUST'])
-          # Apply standard filters
-          customerDf = customerDf.filter(self._internalSrcConfig.__create_filtering_condition__())
+      cchbcData.readData().prepareData()
 
+      return cchbcData._spark
 
-          # Load customer MD PI
-          customerPiDf = spark.read.option("header", "true").option("sep", "|").csv(self._internalSrcConfig.filepaths['customer_pi'].format( CAPS_CC = self._cc.upper()))
+    #def get_internal_source_config(self)->InternalSrcConfig:
+    #  return self._internalSrcConfig
+    
+    #def set_internal_source_config(self, internalSrcConfig:InternalSrcConfig)->None:
+    #  self._internalSrcConfig = internalSrcConfig
+    #def get_external_source_config(self)->ExternalSrcConfig:
+    #  return  self._externalSrcConfig
+    
+    #def set_external_source_config(self)->None:
+    #  self._externalSrcConfig = externalSrcConfig
 
-          # Load country name
-          salesOrgDf = spark.read.option("header", "true").csv(self._internalSrcConfig.filepaths['sales_org'])
-          salesOrgDf = salesOrgDf.select('SALESORG', 'COUNTRYTXT').distinct()
+    #def get_internal_data(self)->DataFrame:
+    #      # Load customer MD
+    #      customerDf = spark.read.option("header", "true").option("sep", "|").csv(self._internalSrcConfig.filepaths['customer'].format( CAPS_CC = self._cc.upper()))
+    #      customerDf = customerDf.fillna("NAN", subset=['_BIC_CSUP_CUST'])
+    #      # Apply standard filters
+    #      customerDf = customerDf.filter(self._internalSrcConfig.__create_filtering_condition__())
+    #      # Load customer MD PI
+    #      customerPiDf = spark.read.option("header", "true").option("sep", "|").csv(self._internalSrcConfig.filepaths['customer_pi'].format( CAPS_CC = self._cc.upper()))
 
-
-          # Create columns
-          self._internalDf = (customerDf.join(customerPiDf, on='CUSTOMER', how='left')
-                                   .join(salesOrgDf, on='SALESORG', how='left')
-                                   .select(customerDf['CUSTOMER'].alias('OUTLET_ID'),
-                                           f.coalesce(customerPiDf['NAME'], customerPiDf['CUSTOMER_DESC']).alias('OUTLET_NAME_1'),
-                                           f.coalesce(customerPiDf['NAME2'], customerPiDf['CUSTOMER_DESC']).alias('OUTLET_NAME_2'),
-                                           salesOrgDf['COUNTRYTXT'].alias('COUNTRY'),
-                                           customerPiDf['CITY'].alias('CITY'),
-                                           customerPiDf['STREET'].alias('ADDRESS'),
-                                           customerPiDf['POSTAL_CD'].alias('POSTAL_CODE'),
-                                           customerDf['LONGITUDE'].cast('double').alias('LONGITUDE'),
-                                           customerDf['LATITUDE'].cast('double').alias('LATITUDE'),
-                                       )
-                       )
-
-          # Add suffix to column names
-          self._internalDf = self._internalDf.select(*[f.col(Col).alias(Col+'_INTERNAL') for Col in self._internalSrcConfig.selected_column_list])
-          return self._internalDf   
-
-    def get_external_data(self)->DataFrame:
-      # Load external outlet data
-        df_from_each_file = (pd.read_csv(f, low_memory=False) for f in [self._externalSrcConfig.poi_filepath[self._cc]])
-        poiListingPd   = pd.concat(df_from_each_file, ignore_index=True)
-
-        # Convert to spark dataframe
-        poiListingDf = spark.createDataFrame(poiListingPd)
-         
+    #      # Load country name
+    #      salesOrgDf = spark.read.option("header", "true").csv(self._internalSrcConfig.filepaths['sales_org'])
+    #      salesOrgDf = salesOrgDf.select('SALESORG', 'COUNTRYTXT').distinct()
           
-        # Filter for at least one review in the last 6 months
-        exprSumCounts = lambda months : pysum( f.coalesce(f.col(f'reviews_count_m{i}').cast('integer'), f.lit(0)) for i in range(1, months+1))
-        poiListingDf = poiListingDf.filter(exprSumCounts(6)>0)
+          # Create columns
+    #      self._internalDf = (customerDf.join(customerPiDf, on='CUSTOMER', how='left')
+    #                              .join(salesOrgDf, on='SALESORG', how='left')
+    #                              .select(customerDf['CUSTOMER'].alias('OUTLET_ID'),
+    #                                      f.coalesce(customerPiDf['NAME'], customerPiDf['CUSTOMER_DESC']).alias('OUTLET_NAME_1'),
+    #                                      f.coalesce(customerPiDf['NAME2'], customerPiDf['CUSTOMER_DESC']).alias('OUTLET_NAME_2'),
+    #                                      salesOrgDf['COUNTRYTXT'].alias('COUNTRY'),
+    #                                      customerPiDf['CITY'].alias('CITY'),
+    #                                      customerPiDf['STREET'].alias('ADDRESS'),
+    #                                      customerPiDf['POSTAL_CD'].alias('POSTAL_CODE'),
+    #                                      customerDf['LONGITUDE'].cast('double').alias('LONGITUDE'),
+    #                                      customerDf['LATITUDE'].cast('double').alias('LATITUDE'),
+    #                                  )
+    #                  )
+    #      # Add suffix to column names
+    #     self._internalDf = self._internalDf.select(*[f.col(Col).alias(Col+'_INTERNAL') for Col in self._internalSrcConfig.selected_column_list])
+    #      return self._internalDf  
 
-        self.set_auxiliary_data(poiListingDf)
-        # Filter for valid coordinates
+    
+
+    #def get_external_data(self)->DataFrame:
+      # Load external outlet data
+    #    df_from_each_file = (pd.read_csv(f, low_memory=False) for f in [self._externalSrcConfig.poi_filepath[self._cc]])
+    #    poiListingPd   = pd.concat(df_from_each_file, ignore_index=True)
+
+    #    # Convert to spark dataframe
+    #    poiListingDf = spark.createDataFrame(poiListingPd)
         
-        poiListingDf = poiListingDf.filter(self._externalSrcConfig.__create_filtering_conditions__())                                                                            
-        poiListingDf = poiListingDf.withColumn('POSTAL_CODE',f.lit(None).cast(t.StringType()))
-        poiListingDf = poiListingDf.withColumn('CITY',f.coalesce(f.col('location_city'), f.col('location_state')))
+        # Filter for at least one review in the last 6 months
+    #    exprSumCounts = lambda months : pysum( f.coalesce(f.col(f'reviews_count_m{i}').cast('integer'), f.lit(0)) for i in range(1, months+1))
+    #    poiListingDf = poiListingDf.filter(exprSumCounts(6)>0)
 
-        # Finalize table
-        mapOfColumnAliases = self._externalSrcConfig.__create_map_of_selected_column_aliases__()
-        self._externalDf = (poiListingDf.select([f.col(colName).alias(aliasName) for colName, aliasName in mapOfColumnAliases.items()]))
+    #    self.set_auxiliary_data(poiListingDf)
+    #    # Filter for valid coordinates
+        
+    #    poiListingDf = poiListingDf.filter(self._externalSrcConfig.__create_filtering_conditions__())                                                                            
+    #    poiListingDf = poiListingDf.withColumn('POSTAL_CODE',f.lit(None).cast(t.StringType()))
+    #    poiListingDf = poiListingDf.withColumn('CITY',f.coalesce(f.col('location_city'), f.col('location_state')))
+
+    #    # Finalize table
+    #    mapOfColumnAliases = self._externalSrcConfig.__create_map_of_selected_column_aliases__()
+    #    self._externalDf = (poiListingDf.select([f.col(colName).alias(aliasName) for colName, aliasName in mapOfColumnAliases.items()]))
